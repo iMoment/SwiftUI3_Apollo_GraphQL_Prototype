1347426921a2cb729dae622f13a63b882b96b9a7
Stanley Pan <imoment.developer@gmail.com>
2022-01-10 14:35:50 -0500

    Initial Commit

---
+++ Apollo_GraphQL/Apollo_GraphQLApp.swift
@@ -0,0 +1,17 @@
+//
+//  Apollo_GraphQLApp.swift
+//  Apollo_GraphQL
+//
+//  Created by Stanley Pan on 2022/01/10.
+//
+
+import SwiftUI
+
+@main
+struct Apollo_GraphQLApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Apollo_GraphQL/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Apollo_GraphQL
+//
+//  Created by Stanley Pan on 2022/01/10.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
